db4a2a27d06a9e2345a2d261422bf1778860f7e2
Vimbai Ruzvidzo <48484802+Vimby@users.noreply.github.com>
2021-08-09 23:50:30 +0200

    Add files via upload
    First Comit

---
+++ index.js
@@ -0,0 +1,33 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
+})
+
+//Slideshow controls
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ 
+  slides[slideIndex-1].style.display = "block";
+
+}
